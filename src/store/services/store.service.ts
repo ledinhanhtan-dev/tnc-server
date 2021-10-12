76417c6b9658f333114f228d2f3d892b69ddecea
Oga <ledinhanhtan.dev@gmail.com>
2021-10-12 15:55:51 +0700

    fix

--- src/store/services/store.service.ts
+++ src/store/services/store.service.ts
@@ -4,36 +4,6 @@ import { Repository } from 'typeorm';
 import { getFeaturedProducts } from '../constants/stores.constant';
 import { Store } from '../entities/store.entity';
 
-const msi: Store = {
-  id: 1,
-  name: 'MSI Official Store',
-  slug: 'msi-official-store',
-  banner: 'http://localhost:3000/image/banner/store-msi-banner.jpg',
-  featuredProducts: [
-    {
-      image: 'http://localhost:3000/image/banner/store-msi-featured-1.jpg',
-      slug: '',
-    },
-    {
-      image: 'http://localhost:3000/image/banner/store-msi-featured-2.png',
-      slug: '',
-    },
-    {
-      image: 'http://localhost:3000/image/banner/store-msi-featured-3.png',
-      slug: '',
-    },
-    {
-      image: 'http://localhost:3000/image/banner/store-msi-featured-4.png',
-      slug: '',
-    },
-    {
-      image: 'http://localhost:3000/image/banner/store-msi-featured-5.png',
-      slug: '',
-    },
-  ],
-  categories: [],
-};
-
 @Injectable()
 export class StoreService {
   constructor(
